da36a0c37fd3d3fc69ae69f7e1eed6d2093c7872
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-10-03 14:35:22 -0400

    update models

--- src/main/java/com/example/socceritemsstore/model/Item.java
+++ src/main/java/com/example/socceritemsstore/model/Item.java
@@ -3,16 +3,11 @@ package com.example.socceritemsstore.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import javax.annotation.processing.Generated;
 import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 public class Item {
 
@@ -30,7 +25,80 @@ public class Item {
     public String imageURL;
     @ElementCollection
     private List<String> sizes;
+    public Item() {}
 
+    // Constructor with fields
+    public Item(String name, String category, Integer quantity, Double price, String gender, String imageURL, List<String> sizes) {
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+        this.price = price;
+        this.gender = gender;
+        this.imageURL = imageURL;
+        this.sizes = sizes;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<String> getSizes() {
+        return sizes;
+    }
+
+    public void setSizes(List<String> sizes) {
+        this.sizes = sizes;
+    }
 }
